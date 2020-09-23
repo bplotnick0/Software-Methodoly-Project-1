3679a22b66ba41a467888f4544a5d18975f5c8ea
msherb5 <msherbs5@gmail.com>
2020-09-22 20:50:24 -0400

    GetSet2

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -9,7 +9,7 @@ public class GroceryItem {
         this.taxable=taxable;
     }
 
-... public boolean equals(Object obj){
+    public boolean equals(Object obj){
         if (!(obj instanceof GroceryItem)){
             return false;
         }else if (((GroceryItem) obj).name == this.name && ((GroceryItem) obj).price == this.price && ((GroceryItem) obj).taxable == this.taxable) {
@@ -24,9 +24,34 @@ public class GroceryItem {
 
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double newPrice){
+        this.price = newPrice;
+    }
+
+    public boolean getTaxable(){
+        return taxable;
+    }
+    public void setTaxable(boolean newTaxable){
+        this.taxable = newTaxable;
+    }
+
     public static void main (String[] args){
-        GroceryItem b = new GroceryItem("meat", "2.99", "false");
+        GroceryItem a = new GroceryItem("meat", 2.99, false);
+        GroceryItem b = new GroceryItem("meat", 2.99, false);
+        GroceryItem c = new GroceryItem("cheese", 1.99, false);
         System.out.println(b.toString());
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
 
 
     }
